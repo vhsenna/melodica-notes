8dd27226a9c3b738320f5e565f23b5448ec91b2a
Victor Senna <vhsenna@gmail.com>
2024-06-05 21:54:56 -0300

    Include chord commands in the CLI

--- melodica_notes/cli.py
+++ melodica_notes/cli.py
@@ -2,20 +2,35 @@ from rich.console import Console
 from rich.table import Table
 from typer import Argument, Typer
 
-from melodica_notes.scales import scale
+from melodica_notes.chords import chord as _chord
+from melodica_notes.scales import scale as _scale
 
 console = Console()
 app = Typer()
 
 
 @app.command()
-def scales(
+def scale(
         tonic_note: str = Argument("C", help="Tonic Note"),
         scale_mode: str = Argument("major", help="Scale Mode")
 ):
     table = Table()
 
-    notes, degrees = scale(tonic_note, scale_mode).values()
+    notes, degrees = _scale(tonic_note, scale_mode).values()
+
+    for degree in degrees:
+        table.add_column(degree)
+
+    table.add_row(*notes)
+
+    console.print(table)
+
+
+@app.command()
+def chord(tonic_note: str = Argument("C", help="Tonic Note")):
+    table = Table()
+
+    notes, degrees = _chord(tonic_note).values()
 
     for degree in degrees:
         table.add_column(degree)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,23 +7,35 @@ runner = CliRunner()
 
 
 def test_scale_cli_should_exit_with_code_0():
-    result = runner.invoke(app)
+    result = runner.invoke(app, ["scale"])
     assert result.exit_code == 0
 
 
 @mark.parametrize('note', ['C', 'D', 'E', 'F', 'G', 'A', 'B'])
-def test_scale_cli_should_contain_correct_scale(note):
-    result = runner.invoke(app)
+def test_scale_cli_should_contain_correct_scale(note,):
+    result = runner.invoke(app, ["scale"])
     assert note in result.stdout
 
 
 @mark.parametrize('note', ['F', 'G', 'A', 'A#', 'C', 'D', 'E'])
 def test_scale_cli_should_contain_correct_scale_for_f(note):
-    result = runner.invoke(app, ['F'])
+    result = runner.invoke(app, ["scale", 'F'])
     assert note in result.stdout
 
 
 @mark.parametrize('degree', ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII'])
 def test_scale_cli_should_contain_all_degrees(degree):
-    result = runner.invoke(app, ['F'])
+    result = runner.invoke(app, ["scale", 'F'])
+    assert degree in result.stdout
+
+
+@mark.parametrize('note', ['C', 'E', 'G'])
+def test_chord_cli_should_contain_correct_response(note,):
+    result = runner.invoke(app, ["chord"])
+    assert note in result.stdout
+
+
+@mark.parametrize('degree', ['I', 'III', 'V'])
+def test_degree_cli_should_contain_correct_response(degree):
+    result = runner.invoke(app, ["scale", 'F'])
     assert degree in result.stdout
