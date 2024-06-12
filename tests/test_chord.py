7b7f6493539b2ef8eecd98e373a9ac64d688564c
Victor Senna <vhsenna@gmail.com>
2024-06-11 23:24:08 -0300

    Implement flat chords

--- tests/test_chord.py
+++ tests/test_chord.py
@@ -11,7 +11,18 @@ from melodica_notes.chords import chord
         ("Cdim", ["C", "D#", "F#"]),
         ("C+", ["C", "E", "G#"]),
         ("Cm+", ["C", "D#", "G#"]),
+
         ("F#", ["F#", "A#", "C#"]),
+        ("F#m", ["F#", "A", "C#"]),
+        ("F#dim", ["F#", "A", "C"]),
+        ("F#+", ["F#", "A#", "D"]),
+        ("F#m+", ["F#", "A", "D"]),
+
+        ("Gb", ["Gb", "Bb", "Db"]),
+        ("Gbm", ["Gb", "A", "Db"]),
+        ("Gbdim", ["Gb", "A", "C"]),
+        ("Gb+", ["Gb", "Bb", "D"]),
+        ("Gbm+", ["Gb", "A", "D"]),
     ]
 )
 def test_chords_return_corresponding_notes(tonic_note, expected):
